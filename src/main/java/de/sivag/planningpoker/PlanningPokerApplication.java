aef2917baa600d477afc2f4952f49a4ccb9280ef
Nico Hoffmann <182205856+fearxinsanity@users.noreply.github.com>
2026-05-19 14:27:17 +0200

    feat: Iteration 16 DB-input deletion after 24h

--- src/main/java/de/sivag/planningpoker/PlanningPokerApplication.java
+++ src/main/java/de/sivag/planningpoker/PlanningPokerApplication.java
@@ -2,12 +2,19 @@ package de.sivag.planningpoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Hauptklasse
+ *
+ * @author Nico Hoffmann
+ * @version 1.0
+ */
 @SpringBootApplication
+@EnableScheduling
 public class PlanningPokerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlanningPokerApplication.class, args);
     }
-
 }
